609747433319463314858ed9c2a9672ec5d65d44
Pragati Sanepara <pragati@atharvasystem.com>
2022-11-18 18:02:44 +0530

    added useContext and useReducer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
-import {
-  UserContextProvider,
-} from "./components/context/UserContext";
+import { UserContext } from "./components/context/UserContext";
+
+const initialState = {
+  step: "",
+  email: "",
+  pauseTime: "",
+  testStatus: "onhold",
+  result: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "UPDATE_USER":
+      localStorage.setItem(
+        "userDetails",
+        JSON.stringify({ ...state, ...action.payload })
+      );
+      return { ...state, ...action.payload };
+    case "GET_USER":
+      let getUser = JSON.parse(localStorage.getItem("userDetails")) || {};
+      return { ...state, ...getUser };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [newUser, dispatch] = React.useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: "GET_USER" });
+  }, []);
+
   return (
     <>
-      <UserContextProvider>
+      <UserContext.Provider
+        value={{
+          newUser: newUser,
+          dispatch: dispatch,
+        }}
+      >
         <BrowserRouter>
           <AppRouter />
         </BrowserRouter>
-      </UserContextProvider>
+      </UserContext.Provider>
     </>
   );
 }
