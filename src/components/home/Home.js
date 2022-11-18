b6ee60d2d2f02f5fa7a55acf19ee563667fe2223
Pragati Sanepara <pragati@atharvasystem.com>
2022-11-18 18:06:40 +0530

    get userData in useEffect and if test is  runing then redirect on test page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,31 +1,32 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 
 function Home() {
   const navigate = useNavigate();
   const location = useLocation();
-  const [user, setUser] = useContext(UserContext);
+  const contextValue = useContext(UserContext);
 
   useEffect(() => {
-    console.log(user);
-    if (user?.email && user?.result) {
-      navigate(`/result`);
-    } else if (user?.testStatus?.toLowerCase() === "inprogress") {
-      navigate('/test');
-    } else if (user?.testStatus?.toLowerCase() === "complete") {
-      navigate("/result");
+    let getData = JSON.parse(localStorage.getItem("userDetails")) || {};
+    contextValue.dispatch({ type: "UPDATE_USER", payload: getData });
+  }, []);
+
+  useEffect(() => {
+    if (contextValue.newUser?.testStatus?.toLowerCase() === "inprogress") {
+      navigate("/test");
     }
-  }, [user, location?.pathname]);
+
+    if (!contextValue.newUser?.step) {
+      let addDetails = {
+        step: "instruction",
+      };
+      contextValue.dispatch({ type: "UPDATE_USER", payload: addDetails });
+    }
+  }, [contextValue.newUser, location?.pathname]);
 
   const handleSubmit = () => {
-    if (user?.email && user?.result) {
-      navigate(`/result`);
-    } else if (user?.email && !user?.result) {
-      navigate(`/success`);
-    } else {
-      navigate(`/register`);
-    }
+    navigate(`/register`);
   };
 
   return (
