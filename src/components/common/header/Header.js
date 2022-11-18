af60685904ee2ea1bcd2709902ac2757a69a44a2
Pragati Sanepara <pragati@atharvasystem.com>
2022-11-18 18:04:46 +0530

    remove unused code

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -18,24 +18,6 @@ function Header() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarText">
-            {/* <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-            </ul> */}
-            {/* <span class="navbar-text">Navbar text with an inline element</span> */}
           </div>
         </div>
       </nav>
