78f1f4dd4e6019393388012ea798407675816e9f
Pragati Sanepara <pragati@atharvasystem.com>
2022-11-18 18:05:01 +0530

    craeted only context

--- src/components/context/UserContext.js
+++ src/components/context/UserContext.js
@@ -1,27 +1,9 @@
-import React, { createContext, useEffect, useReducer, useState } from "react";
+import React from "react";
 
-// create context
-const UserContext = createContext(null);
-
-const UserContextProvider = ({ children }) => {
-  // the value that will be given to the context
-  const [user, setUser] = useState({});
-  // const [state, dispatch] = useReducer(reducer, initialState);
-
-  useEffect(() => {
-    setUser(JSON.parse(localStorage.getItem("userDetails")) || {});
-  }, []);
-
-  useEffect(() => {
-    console.log('test====', user);
-  }, [user])
-
-  return (
-    // the Provider gives access to the context to its children
-    <UserContext.Provider value={[user, setUser]}>
-      {children}
-    </UserContext.Provider>
-  );
-};
-
-export { UserContext, UserContextProvider };
+export const UserContext = React.createContext({
+  step: "",
+  email: "",
+  pauseTime: "",
+  testStatus: "onhold",
+  result: null,
+});
