097909b046f09e27b92e6827c5af7b241f57ea09
Pragati Sanepara <pragati@atharvasystem.com>
2022-11-18 18:07:49 +0530

    get userData in useEffect and if test runing then redirect to test

--- src/components/result/Result.js
+++ src/components/result/Result.js
@@ -1,25 +1,34 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 
 function Result() {
-  const [user, setUser] = useContext(UserContext);
+  const contextValue = useContext(UserContext);
   const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!user?.email) {
-      navigate("/register");
-    } else if (user?.testStatus?.toLowerCase() === "inprogress") {
+    if (contextValue.newUser?.testStatus?.toLowerCase() === "inprogress") {
       navigate("/test");
-    } else if (user?.testStatus?.toLowerCase() === "onhold") {
-      navigate("/success");
     }
-  }, [user, location]);
+
+    if (contextValue.newUser?.email && contextValue.newUser?.step === "quiz") {
+      let addDetails = {
+        step: "final",
+      };
+      contextValue.dispatch({ type: "UPDATE_USER", payload: addDetails });
+    }
+
+    if (!contextValue.newUser?.step) {
+      navigate("/");
+    } else if (contextValue.newUser?.step === "instruction") {
+      navigate("/register");
+    }
+  }, [contextValue.newUser, location?.pathname]);
 
   return (
     <>
-      <h1>Result {user?.result}</h1>
+      <h1>Result {contextValue.newUser?.result}</h1>
     </>
   );
 }
