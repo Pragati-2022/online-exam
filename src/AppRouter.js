0cf5df8767aeee47e22ec2d35d8b3c4c2259fc35
Pragati Sanepara <pragati@atharvasystem.com>
2022-11-20 18:08:49 +0530

    added Wildcard route and related component

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Registration from "./components/basic/Registration/Registration";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Success from "./components/success/Success";
 import Test from "./components/test/Test";
@@ -8,10 +8,20 @@ import Header from "./components/common/header/Header";
 import Result from "./components/result/Result";
 import Dashboard from "./components/dashboard/Dashboard";
 import Home from "./components/home/Home";
+import { UserContext } from "./components/context/UserContext";
+
+function NotFound() {
+  return <h3>Not Found</h3>;
+}
 
 function AppRouter() {
   const location = useLocation();
   const [isStartTest, setStartTest] = useState(false);
+  const contextValue = useContext(UserContext);
+
+  useEffect(() => {
+    contextValue.dispatch({ type: "GET_USER"});
+  }, []);
 
   useEffect(() => {
     setStartTest(
@@ -31,6 +41,7 @@ function AppRouter() {
         <Route path="/test" element={<Test />} />
         <Route path="/test/:id" element={<Test />} />
         <Route path="/result" element={<Result />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
