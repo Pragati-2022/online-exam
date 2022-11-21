70e2c3dedb9ea8e693ae324743636cb1bcaca509
Pragati Sanepara <pragati@atharvasystem.com>
2022-11-21 19:34:59 +0530

    In  NotFound component condition for if user on quiz page then redirect to quiz Dashboard shown only 3 steps

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,31 +1,40 @@
 import "./App.css";
 import Registration from "./components/basic/Registration/Registration";
 import React, { useContext, useEffect, useState } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
-import Success from "./components/success/Success";
-import Test from "./components/test/Test";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import StartTest from "./components/start-test/StartTest";
+import Quiz from "./components/quiz/Quiz";
 import Header from "./components/common/header/Header";
 import Result from "./components/result/Result";
 import Dashboard from "./components/dashboard/Dashboard";
-import Home from "./components/home/Home";
 import { UserContext } from "./components/context/UserContext";
+import Instruction from "./components/instruction/Instruction";
 
 function NotFound() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const contextValue = useContext(UserContext);
+  useEffect(() => {
+    if (contextValue.newUser?.testStatus?.toLowerCase() === "inprogress") {
+      navigate("/quiz");
+    }
+  }, [contextValue.newUser, location?.pathname]);
+
   return <h3>Not Found</h3>;
 }
 
-function AppRouter() {
+function AppRouter(props) {
   const location = useLocation();
   const [isStartTest, setStartTest] = useState(false);
   const contextValue = useContext(UserContext);
 
   useEffect(() => {
-    contextValue.dispatch({ type: "GET_USER"});
+    contextValue.dispatch({ type: "GET_USER" });
   }, []);
 
   useEffect(() => {
     setStartTest(
-      location.pathname.includes("/test") ||
+      location.pathname.includes("/quiz") ||
         location.pathname.includes("/result")
     );
   }, [location]);
@@ -33,15 +42,38 @@ function AppRouter() {
   return (
     <>
       <Header />
-      {!isStartTest && <Dashboard />}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Registration />} />
-        <Route path="/success" element={<Success />} />
-        <Route path="/test" element={<Test />} />
-        <Route path="/test/:id" element={<Test />} />
+        <Route
+          path="/"
+          element={
+            <React.Fragment>
+              <Dashboard />
+              <Instruction />
+            </React.Fragment>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <React.Fragment>
+              <Dashboard />
+              <Registration />
+            </React.Fragment>
+          }
+        />
+        <Route
+          path="/star_test"
+          element={
+            <React.Fragment>
+              <Dashboard />
+              <StartTest />
+            </React.Fragment>
+          }
+        />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/quiz/:id" element={<Quiz />} />
         <Route path="/result" element={<Result />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFound status={404} />} />
       </Routes>
     </>
   );
