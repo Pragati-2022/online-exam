4a09641d32f034feecf8a16f44a92b4af4c245ee
Pragati Sanepara <pragati@atharvasystem.com>
2022-11-23 18:43:51 +0530

    added istruction  and  if user enter test then that cant access this again

--- src/components/instruction/Instruction.js
+++ src/components/instruction/Instruction.js
@@ -15,6 +15,8 @@ function Instruction() {
   useEffect(() => {
     if (contextValue.newUser?.testStatus?.toLowerCase() === "inprogress") {
       navigate("/quiz");
+    } else if (contextValue.newUser?.testStatus?.toLowerCase() === "complete") {
+      navigate("/result");
     }
 
     if (!contextValue.newUser?.step) {
@@ -22,7 +24,11 @@ function Instruction() {
         step: "instruction",
       };
       contextValue.dispatch({ type: "UPDATE_USER", payload: addDetails });
-    }
+    } else if (contextValue.newUser?.step === "enter_details") {
+      navigate("/register");
+    } else if (contextValue.newUser?.step === "start_test") {
+      navigate("/start_test");
+    } 
   }, [contextValue.newUser, location?.pathname]);
 
   const handleSubmit = () => {
@@ -34,17 +40,40 @@ function Instruction() {
       <div className="homepage-main">
         <div className="container">
           <div className="homepage-inner">
-            <h2>Home page Here</h2>
+            <h2>React JS Quiz</h2>
+            <ul style={{ listStyleType: "none" }}>
+              <li>
+                By <strong>Atharva System</strong>
+              </li>
+              <li>
+                Duration <strong>60 mins</strong>
+              </li>
+            </ul>
           </div>
+          <div className="homepage-inner">
+            <h3>General Instructions</h3>
+            <ul>
+              <li>Test duration : 60 mins</li>
+              <li>
+                Duration <strong>60 Minutes</strong>
+              </li>
+              <li>
+                Click <strong>End Test</strong> after you answer each questions
+              </li>
+              <li>
+                <strong>30</strong> Questions
+              </li>
+            </ul>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-success"
+            onClick={() => handleSubmit()}
+          >
+            Enter Test
+          </button>
         </div>
       </div>
-      <button
-        type="submit"
-        className="btn btn-success"
-        onClick={() => handleSubmit()}
-      >
-        Enter Test
-      </button>
     </>
   );
 }
