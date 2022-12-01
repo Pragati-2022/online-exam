60441b8a91fd28595bd4fddc6d65a2e2fb5cff52
Pragati Sanepara <pragati@atharvasystem.com>
2022-12-01 10:38:29 +0530

    added mobileNo, collegeName and experience and spinner, api integration

--- src/components/basic/Registration/Registration.js
+++ src/components/basic/Registration/Registration.js
@@ -2,17 +2,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useRef, useEffect, useContext } from "react";
 import { UserContext } from "./../../context/UserContext";
 import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../../../images/ring-36.svg";
 
 function Registration() {
   const navigate = useNavigate();
   const location = useLocation();
   const [success, setSuccess] = useState("");
   const [error, setError] = useState("");
+  const [isLoad, setIsLoad] = useState(false);
+  const [collegeNames, setCollegeNames] = useState([]);
 
   const firstNameRef = useRef(null);
   const middleNameRef = useRef(null);
   const lastNameRef = useRef(null);
   const emailRef = useRef(null);
+  const phoneRef = useRef(null);
+  const collegeNameRef = useRef(null);
+  const experienceRef = useRef(null);
   const dateOfBirthRef = useRef(null);
   const educationDetailsRef = useRef(null);
   const areaOfInterestRef = useRef(null);
@@ -21,31 +28,19 @@ function Registration() {
   const contextValue = useContext(UserContext);
 
   useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API}/users/college/get`).then((res) => {
+      setCollegeNames(res?.data);
+    });
+  }, []);
+
+  useEffect(() => {
     if (contextValue.newUser?.testStatus?.toLowerCase() === "inprogress") {
       navigate("/quiz");
-    }  else if (contextValue.newUser?.testStatus?.toLowerCase() === "complete") {
+    } else if (contextValue.newUser?.testStatus?.toLowerCase() === "complete" ||  contextValue.newUser?.step?.toLowerCase() === "final" ) {
       navigate("/result");
-    }
+    } 
 
-    if (contextValue.newUser?.details) {
-      firstNameRef.current.value =
-        contextValue.newUser?.details.fullName.firstName;
-      middleNameRef.current.value =
-        contextValue.newUser?.details.fullName.middleName;
-      lastNameRef.current.value =
-        contextValue.newUser?.details.fullName.lastName;
-      emailRef.current.value = contextValue.newUser?.details.email;
-      dateOfBirthRef.current.value = contextValue.newUser?.details.dateOfBirth;
-      educationDetailsRef.current.value =
-        contextValue.newUser?.details.educationDetails;
-      areaOfInterestRef.current.value =
-        contextValue.newUser?.details.areaOfInterest;
-      futureGoalRef.current.value = contextValue.newUser?.details.futureGoal;
-      currentAddressRef.current.value =
-        contextValue.newUser?.details.currentAddress;
-    }
-
-    if (contextValue.newUser?.step === "instruction") {
+    if (!contextValue.newUser?.step) {
       let addDetails = {
         step: "enter_details",
       };
@@ -53,11 +48,7 @@ function Registration() {
     } else if (contextValue.newUser?.step === "start_test") {
       navigate("/start_test");
     }
-
-    if (!contextValue.newUser?.step) {
-      navigate("/");
-    }
-  }, [contextValue.newUser, location?.pathname]);
+  }, [contextValue.newUser]);
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -66,6 +57,9 @@ function Registration() {
       !middleNameRef.current.value ||
       !lastNameRef.current.value ||
       !emailRef.current.value ||
+      !phoneRef.current.value ||
+      !collegeNameRef.current.value ||
+      !experienceRef.current.value ||
       !dateOfBirthRef.current.value ||
       !educationDetailsRef.current.value ||
       !areaOfInterestRef.current.value ||
@@ -75,30 +69,64 @@ function Registration() {
       setSuccess("");
       setError("Details are required!");
     } else {
-      setSuccess("Register successful");
-      setError("");
-
-      navigate(`/start_test`);
-
-      let userDetail = {
-        fullName: {
-          firstName: firstNameRef.current.value,
-          middleName: middleNameRef.current.value,
-          lastName: lastNameRef.current.value,
-        },
+      const userDetail = {
+        firstName: firstNameRef.current.value,
+        middleName: middleNameRef.current.value,
+        lastName: lastNameRef.current.value,
         email: emailRef.current.value,
-        dateOfBirth: dateOfBirthRef.current.value,
+        mobileNo: phoneRef.current.value,
+        collegeName: collegeNameRef.current.value,
+        experience: experienceRef.current.value,
+        dob: dateOfBirthRef.current.value,
         educationDetails: educationDetailsRef.current.value,
-        areaOfInterest: areaOfInterestRef.current.value,
+        areaOfIntrest: areaOfInterestRef.current.value,
         futureGoal: futureGoalRef.current.value,
         currentAddress: currentAddressRef.current.value,
+        // collegeName : 'JECRC'
       };
 
-      let addDetails = {
-        email: emailRef.current.value,
-        details: userDetail,
-      };
-      contextValue.dispatch({ type: "UPDATE_USER", payload: addDetails });
+      setIsLoad(true);
+      axios
+        .post(`${process.env.REACT_APP_API}/users/candidate/create`, userDetail)
+        .then((res) => {
+          console.log("create user", res);
+          setSuccess("Register successful");
+          setError("");
+          if (
+            experienceRef.current.value === "0" ||
+            experienceRef.current.value === "1-2"
+          ) {
+            const addDetails = {
+              email: emailRef.current.value,
+            };
+            contextValue.dispatch({ type: "UPDATE_USER", payload: addDetails });
+            navigate(`/start_test`);
+          } else {
+            const addDetails = {
+              step: "final",
+              email: emailRef.current.value,
+            };
+            contextValue.dispatch({ type: "UPDATE_USER", payload: addDetails });
+            navigate(`/result`);
+          }
+          setIsLoad(false);
+        })
+        .catch((error) => {
+          setSuccess("");
+          setError(error.response.data.message);
+          alert(error.response.data.message);
+          setIsLoad(false);
+          return false;
+        });
+    }
+  };
+
+  const handleOnChangePhone = (e) => {
+    const re = /^[0-9\b]+$/;
+    if (e.target.value === "" || re.test(e.target.value)) {
+      phoneRef.current.value = e.target.value;
+    } else {
+      phoneRef.current.value = "";
     }
   };
 
@@ -166,7 +194,7 @@ function Registration() {
               </div>
             </div>
             <div className="form-group ">
-              <label htmlFor="">Email Address</label>
+              <label htmlFor="email">Email Address</label>
               <div className="form-group-inner">
                 <div className="cmn-form-control">
                   <input
@@ -178,6 +206,68 @@ function Registration() {
                     required
                     pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
                   />
+                </div>
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Mobile No.</label>
+              <div className="form-group-inner">
+                <div className="cmn-form-control">
+                  <input
+                    id="phone"
+                    placeholder="Mobile No"
+                    type="text"
+                    className="form-control"
+                    onChange={(e) => handleOnChangePhone(e)}
+                    ref={phoneRef}
+                    minLength="10"
+                    maxLength="10"
+                    required
+                  />
+                </div>
+              </div>
+            </div>{" "}
+            <div className="form-group">
+              <label htmlFor="collegeName">College Name</label>
+              <div className="form-group-inner">
+                <div className="cmn-form-control">
+                  <select
+                    name="collegeName"
+                    id="collegeName"
+                    className="form-control"
+                    ref={collegeNameRef}
+                    required
+                  >
+                    <option value="">Select College Name</option>
+                    {collegeNames.map((obj, index) => (
+                      <option key={index} value={obj.collegeName}>
+                        {obj.collegeName}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="experience">Experience</label>
+              <div className="form-group-inner">
+                <div className="cmn-form-control">
+                  <select
+                    name="experience"
+                    id="experience"
+                    className="form-control"
+                    ref={experienceRef}
+                    required
+                  >
+                    <option value="">Select Experience</option>
+                    <option value="0">0</option>
+                    <option value="1-2">1-2</option>
+                    <option value="3-4">3-4</option>
+                    <option value="5-6">5-6</option>
+                    <option value="7-8">7-8</option>
+                    <option value="9-10">9-10</option>
+                    <option value="10+">10+</option>
+                  </select>
                 </div>
               </div>
             </div>
@@ -240,8 +330,10 @@ function Registration() {
               </div>
             </div>
             <div className="form-group">
-              <button type="submit" className="cmn-btn submit-btn">
-                Submit
+              <button type="submit" className="cmn-btn submit-btn" disabled={isLoad}>
+              {
+                !isLoad ?  <span>Submit</span> :  <span><img src={Spinner} alt="Spinner" />Loading...</span>
+              } 
               </button>
             </div>
           </div>
