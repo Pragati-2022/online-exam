6b9953602ca82eaf4a28bd696ca628b5d8073bcf
Pragati Sanepara <pragati@atharvasystem.com>
2022-12-01 17:12:34 +0530

    added query filed and its ans store logic

--- src/components/questions/Questions.js
+++ src/components/questions/Questions.js
@@ -1,14 +1,16 @@
-import { useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { QuestionContext } from "../context/QuestionsContext";
 
 function Questions(props) {
   const params = useParams();
   const navigate = useNavigate();
+  const questionContextValue = useContext(QuestionContext);
 
   useEffect(() => {
     if (
       JSON.stringify(params) !== "{}" &&
-      params["id"] !== props.questions?.index
+      params["id"] !== props.question?.index
     ) {
       navigate("/test/1");
     }
@@ -22,7 +24,6 @@ function Questions(props) {
       ...props.question,
       options: options,
     };
-
     props.updateQuestion(question);
   };
 
@@ -39,37 +40,63 @@ function Questions(props) {
     props.updateQuestion(question);
   };
 
+  const handleOnChangeQuery = (index, e, id) => {
+    console.log(e);
+    let options = props.question.options;
+    options.map((data) => {
+      data.query = e.target.value;
+      return (data.value = data.query ? true : false);
+    });
+
+    let question = {
+      ...props.question,
+      options: options,
+    };
+    props.updateQuestion(question);
+  };
+
   return (
     <>
+      {/* {console.log(props.question)} */}
       <div className="question-list-main">
         <div className="q-id"> Question {props.question?.index + 1}</div>
-        <div className="q-title">{props.question?.title}</div>
-        <div className="q-marks">Marks: {props.question?.marks}</div>
+        <div className="q-title">{props.question?.question}</div>
+        {props.question?.marks ? (
+          <div className="q-marks">Marks: {props.question?.marks}</div>
+        ) : null}
         <ul className="question-list">
           {props.question?.options?.map((option, index) => {
             return (
-              <li key={`${index}-${props.question?.id}`}>
-                {props.question?.isMultiAns ? (
+              <li key={`${index}-${props.question?._id}`}>
+                {props.question?.optionType === "Multiple" ? (
                   <input
                     type="checkbox"
-                    id={`custom-checkbox-${index}-${props.question?.id}`}
+                    id={`custom-checkbox-${index}-${props.question?._id}`}
                     name={option?.title}
                     checked={option?.value}
                     onChange={(e) => handleOnChangeCheckbox(index, e)}
                   />
+                ) : props.question?.optionType === "Query" 
+                ? (
+                  <textarea
+                    id={`custom-checkbox-${index}-${props.question?._id}`}
+                    name={option?._id}
+                    defaultValue={option?.query}
+                    onChange={(e) => handleOnChangeQuery(index, e, option._id)}
+                  ></textarea>
                 ) : (
                   <input
                     type="radio"
                     value={option?.title}
-                    id={`custom-checkbox-${index}-${props.question?.id}`}
-                    name="gender"
+                    id={`custom-checkbox-${index}-${props.question?._id}`}
+                    name={option?.title}
                     checked={option?.value}
                     onChange={(e) => handleOnChangeRadio(index, e)}
                   />
                 )}
 
                 <label
-                  htmlFor={`custom-checkbox-${index}-${props.question?.id}`}
+                  htmlFor={`custom-checkbox-${index}-${props.question?._id}`}
                 >
                   {option?.title}
                 </label>
