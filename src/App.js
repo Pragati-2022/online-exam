85136443ef47af18e72bec6501aa859b740327d3
Pragati Sanepara <pragati@atharvasystem.com>
2022-12-01 17:10:18 +0530

    added QuestionContext and reducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
 import { UserContext } from "./components/context/UserContext";
+import { QuestionContext } from "./components/context/QuestionsContext";
+import axios from "axios";
 
 const initialState = {
   step: "",
@@ -28,10 +30,46 @@ function reducer(state, action) {
   }
 }
 
+function questionReducer(state, action) {
+  switch (action.type) {
+    case "UPDATE_QUESTION":
+      if (state.length) {
+        let index = state.findIndex(
+          (data) => data.questionId === action.payload.questionId
+        );
+        if (index >= 0) {
+          state[index] = action.payload;
+        } else {
+          state = [...state, action.payload];
+        }
+      } else {
+        state = [action.payload];
+      }
+      return [...state];
+    case "CREATE_QUESTION":
+      if (!state.length) {
+        state = action.payload;
+      }
+      return [...state];
+    case "END_TEST":
+      console.log(action.payload);
+      axios
+        .post(
+          `${process.env.REACT_APP_API}/users/test/submit`,
+          action.payload
+        )
+        .then((res) => {
+          console.log(res);
+        });
+      return [...state];
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [newUser, dispatch] = useReducer(
-    reducer, initialState
-  );
+  const [newUser, dispatch] = useReducer(reducer, initialState);
+  const [question, questionDispatch] = useReducer(questionReducer, []);
 
   useEffect(() => {
     dispatch({ type: "GET_USER" });
@@ -45,9 +83,16 @@ function App() {
           dispatch: dispatch,
         }}
       >
-        <BrowserRouter>
-          <AppRouter />
-        </BrowserRouter>
+        <QuestionContext.Provider
+          value={{
+            question: question,
+            questionDispatch: questionDispatch,
+          }}
+        >
+          <BrowserRouter>
+            <AppRouter />
+          </BrowserRouter>
+        </QuestionContext.Provider>
       </UserContext.Provider>
     </>
   );
